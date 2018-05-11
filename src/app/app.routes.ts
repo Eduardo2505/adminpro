b586c9f81c3126d7e76069b9796c2c460deb327f
Eduardo Padilla Cruz <eduardopadillacz@gmail.com>
2018-05-11 01:57:41 -0500

    registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,25 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
+
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
+
 const appRoutes: Routes = [
-    {path:'dashboard',component:DashboardComponent},
+    {
+      path:'',
+      component:PagesComponent,
+      children:[
+        {path:'dashboard',component:DashboardComponent},
+        {path:'progress',component:ProgressComponent},
+        {path:'grafias1',component:Graficas1Component},
+        {path:'',redirectTo:'dashboard',pathMatch:'full'}
+      ]
+  },
+    
     {path:'login',component:LoginComponent},
-    {path:'register',component:DashboardComponent},
-    {path:'progess',component:ProgressComponent},
-    {path:'grafias1',component:Graficas1Component},
-    {path:'',redirectTo:'dashboard',pathMatch:'full'},
-    {path:'**',component:NopagefoundComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'**',component:NopagefoundComponent}
   ];
 
   export const APP_ROUTES=RouterModule.forRoot(appRoutes,{useHash:true})
